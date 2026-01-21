92d2b4c1f0e5506bc31c113a20adc38e31c871c3
guisfons <guilhermesfonsecaa@gmail.com>
2026-01-21 11:28:38 -0300

    fix: changed order of events

--- src/app/(public)/eventos/eventos-client.tsx
+++ src/app/(public)/eventos/eventos-client.tsx
@@ -19,8 +19,8 @@ export default function Home() {
       <Program />
       <EducationTrails />
       <CommercialEducationTrack />
-      <EventsPast />
       <Events />
+      <EventsPast />
       <Footer />
     </>
   );
